5f8517cba1db69a9a8167ce3d138ff5cc10421f8
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-02 11:16:02 +0000

    adding test for starting with no records

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 
 describe( "Record Store", function() {
 
+    var emptyRecordShop = null;
     var abbeyRoad = null;
     var batOutOfHell = null;
     var tooDarkPark = null;
@@ -9,11 +10,16 @@ describe( "Record Store", function() {
     var parklife = null;
 
     beforeEach( function() {
-      
+
+      emptyRecordShop = new RecordShop();
       abbeyRoad = new Record( "Abbey Road", "The Beatles", 15.99 );
       batOutOfHell = new Record( "Bat Out of Hell", "Meatloaf", 12.99 );
       tooDarkPark = new Record( "Too Dark Park", "Skinny Puppy", 13.50 );
       slipperyWhenWet = new Record( "Slippery When Wet", "Bon Jovi", 12.99 );
       parklife = new Record( "Parklife", "Blur", 10.00 );
+    });
+
+    it( "Should start with no records", function() {
+      assert.equal( 0, emptyRecordShop.records.length );
     });
 });
