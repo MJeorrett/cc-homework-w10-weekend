84925d5200f03ff645118668fc1df01d6714689a
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-02 11:21:40 +0000

    adding stockLevel function

--- record_shop.js
+++ record_shop.js
@@ -3,4 +3,10 @@ var RecordShop = function() {
   this.records = [];
 };
 
+RecordShop.prototype = {
+  stockLevel: function() {
+    return this.records.length;
+  }
+};
+
 module.exports = RecordShop;
