f310f0989517491967ce274c8543ed4e931ae61f
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-02 10:46:49 +0000

    adding test for album artist

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -12,4 +12,8 @@ describe( "Record", function() {
   it( "Should have name passed in constructor", function() {
     assert.equal( "Abbey Road", abbeyRoad.name );
   });
+
+  it( "Should have artist passed in constructor", function() {
+    assert.equal( "The Beatles", abbeyRoad.artist );
+  });
 });
